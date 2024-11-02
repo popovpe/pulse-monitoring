e6817dbe09527fc313bb8670979faa547f09c683
Petr Popov <popov.pe@gmai.com>
2024-11-02 20:07:31 +0200

    CW #64 sam project Initial code

---
+++ pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -0,0 +1,15 @@
+package telran.pulse.monitoring;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+
+public class App {
+	 public void handleRequest(final DynamodbEvent event, final Context context) {
+	        event.getRecords().forEach( r -> {
+	        	var map = r.getDynamodb().getNewImage();
+	        	System.out.printf("Patient ID = %s, timestamp = %s, pulse = %s%n",
+	        			map.get("patientId").getN(), map.get("timestamp").getN(), map.get("value").getN());
+	        });
+	    }
+
+}
